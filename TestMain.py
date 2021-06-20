12bff8c5cc49e039881504ed0b83b72d9bc97ebe
anandpadhi <anand@infinodo.com>
2021-06-20 00:34:19 -0700

    First Commit

---
+++ TestMain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 20 00:06:18 2021
+
+@author: anandpadhi
+"""
+
+class A:
+    def __init__(self, a, b):
+        self.aV = a
+        self.bV = b
+        
+    @staticmethod 
+    def statMethod(i,a):
+        i ^= 5
+        a *= 6
+        return a//i
+    
+a = A()
+a.statMethod(4, 24)
